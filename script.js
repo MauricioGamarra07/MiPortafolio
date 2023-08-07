f8a9a228b8ba815ebe552615d590d32ac5a2c7a1
MauricioGamarra07 <mauriciogamarra01@gmail.com>
2023-08-07 15:43:56 -0500

    agregando proyectos

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-function llamar(){
-    let botonera=document.getElementById("botonera");
-    botonera.classList.toggle("botonera-open");
+function llamar() {
+  let botonera = document.getElementById("botonera");
+  botonera.classList.toggle("botonera-open");
 
-    let menu=document.getElementById("menu");
-    menu.querySelector('.menu .cerrar').classList.toggle("fa-times");
+  let menu = document.getElementById("menu");
+  menu.querySelector('.menu .cerrar').classList.toggle("fa-times");
 }
 
 //Header
-window.addEventListener("scroll",Mover)
-function Mover(){
-    let header=document.querySelector(".cont-header");
-    let pos = window.scrollY;
-    /* console.log(pos); */
-    if (pos>600){
-        header.classList.add("efecto");
-    }
-    else{
-        header.classList.remove("efecto");
-    }
+window.addEventListener("scroll", Mover)
+function Mover() {
+  let header = document.querySelector(".cont-header");
+  let pos = window.scrollY;
+  /* console.log(pos); */
+  if (pos > 600) {
+    header.classList.add("efecto");
+  }
+  else {
+    header.classList.remove("efecto");
+  }
 }
+
